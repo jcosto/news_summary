9fe19130a4d52af19b6dae94897c597f254777ff
jcosto <jcosto@somnomed.com>
2021-09-23 00:17:22 +0800

    add cnn

---
+++ news_sources/news_cnn.py
@@ -0,0 +1,139 @@
+from news import NewsGroup, NewsItem, has_class_name, get_page_source, get_filesafe_url
+from news import html_dir, json_dir
+import bs4
+import os
+import datetime
+
+class NewsItem_CNN(NewsItem):
+    URLS = [
+        "https://edition.cnn.com/business"
+    ]
+    @classmethod
+    def yield_news(cls, soup: bs4.BeautifulSoup, base_url: str):
+        for s in soup.find('ul', {"class", "cn-list-hierarchical-xs"}).findAll("li"):
+            # print(s["class"])
+            # print(['StoryCollection__story' in c for c in s["class"]])
+            # print(s)
+            # print(s)
+            print(s.find('a')['href'])
+            n = NewsItem_CNN(
+                base_url,
+                base_url + s.find('a')['href'],
+                "",
+                s.find("span", {"class": "cd__headline-text"}).text,
+                s.find('article',['data-section-name'])
+            )
+            print(s.find("span", {"class": "cd__headline-text"}).text)
+            yield n
+    @classmethod
+    def cleanup_content(cls, content: list):
+        return content
+    
+    @classmethod
+    def cleanup_content_item(cls, c: str, i: int):
+        return c
+
+    def extract_header(self, soup: bs4.BeautifulSoup):
+        self.header = soup.find("h1", {"class", "pg-headline"}).text
+    
+    def extract_full_date(self, soup: bs4.BeautifulSoup):
+        self.full_date = soup.find("p", {"class", "update-time"}).text
+        print(self.full_date)
+    
+    def extract_summary_content(self, soup: bs4.BeautifulSoup):
+        summary = list()
+        content = list()
+        from news import has_class_name
+        d = soup.find("div", {"class": "pg-rail-tall__body"})
+        # print(d)
+        if not d:
+            d = soup.find("div", {"class": "pg-special-article__wrapper"})
+        if not d:
+            return
+        d = d.find("div", {"class": "l-container"})
+        for d in d.findAll("div"):
+            this_text = None
+            if has_class_name(d, ['el__leafmedia']):
+                p = d.find('p', {"class": 'zn-body__paragraph'})
+                if p:
+                    for c in p.find_all("cite"):
+                        c.decompose()
+                    this_text = p.get_text()
+            if has_class_name(d, ['zn-body__paragraph']):
+                this_text = d.text
+            if not this_text is None:
+                if 'A version of this story first appeared in CNN' in this_text:
+                    this_text = None
+            
+            if not this_text is None:
+                if len(this_text) == 0:
+                    continue
+                if len(summary) < 2:
+                    print(this_text)
+                    summary.append(this_text)
+                content.append(this_text)
+        
+        self.summary = ' '.join(summary)
+        print("summary", self.summary)
+        
+        self.content = list(content)
+        self.content_raw = list(content)
+
+        # print("content", self.content)
+
+    def extract_news_content(
+        self,
+        news_content_html_dir,
+        sleep_s=0, hold_proc=True
+    ):
+        url = self.url
+        page_source = get_page_source(
+            url, os.path.join(news_content_html_dir, get_filesafe_url(url)),
+            sleep_s=sleep_s, hold_proc=hold_proc
+        )
+        soup = bs4.BeautifulSoup(page_source, features='html.parser')
+                
+        self.extract_header(soup)
+
+        self.extract_full_date(soup)
+
+        self.extract_summary_content(soup)
+        
+    def cleanup_data(self):
+        print(' '.join(self.full_date.split()[-3:]))
+        # input("enter to continue")
+        self.date = datetime.datetime.strptime(' '.join(self.full_date.split()[-3:]),r"%B %d, %Y").isoformat()
+        self.content = NewsItem_CNN.cleanup_content(self.content)
+
+def process_item(n: NewsItem_CNN):
+    print("------------")
+    print(n.base_url)
+    print(n.url)
+    n.extract_news_content(html_dir, hold_proc=False)
+    n.cleanup_data()
+    
+    for i, j in zip(n.content, n.content_raw):
+        print("-- " + i)
+        print("++ " + j)
+    # input("enter to continue...")
+
+from threading import Thread
+def process(front_url):
+    front_filesafe = front_url.replace(":","_").replace("/","_").replace(".","_")
+    front_html = os.path.join(html_dir, front_filesafe)
+    front_json = os.path.join(json_dir, front_filesafe)
+    ng = NewsGroup("https://edition.cnn.com", front_url, front_html, front_json)
+    t_list = list()
+    for n in ng.extract_soup(NewsItem_CNN.yield_news, hold_proc=False):
+        t = Thread(target=process_item, args=[n], daemon=True)
+        t_list.append(t)
+        t.start()
+    # for t in t_list:
+        t.join()
+
+    ng.save_json()
+    return front_html, front_json
+
+if __name__ == "__main__":
+    for front_url in NewsItem_CNN.URLS:
+        html_path, json_path = process(front_url)
